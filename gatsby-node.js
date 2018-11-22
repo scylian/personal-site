a19c9219e3ac572a6a67efaee1e578307e097802
Bien Nguyen <bien@Biens-MacBook-Pro.local>
2018-11-21 23:56:50 -0700

    gallery page

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,7 +1,31 @@
-/**
- * Implement Gatsby's Node APIs in this file.
- *
- * See: https://www.gatsbyjs.org/docs/node-apis/
- */
+const path = require('path');
+const _ = require('lodash');
 
-// You can delete this file if you're not using it
+exports.onCreateNode = ({ node, actions }) => {
+  const { createNodeField } = actions;
+  let slug;
+  if (node.internal.type === 'MarkdownRemark') {
+    if (
+      Object.prototype.hasOwnProperty.call(node, 'frontmatter') &&
+      Object.prototype.hasOwnProperty.call(node.frontmatter, 'slug')
+    ) {
+      slug = `/${_.kebabCase(node.frontmatter.slug)}`;
+    }
+    if (
+      Object.prototype.hasOwnProperty.call(node, 'frontmatter') &&
+      Object.prototype.hasOwnProperty.call(node.frontmatter, 'title')
+    ) {
+      slug = `/${_.kebabCase(node.frontmatter.title)}`;
+    }
+    createNodeField({ node, name: 'slug', value: slug });
+  }
+};
+
+/* Allow us to use something like: import { X } from 'directory' instead of '../../folder/directory' */
+exports.onCreateWebpackConfig = ({ stage, actions }) => {
+  actions.setWebpackConfig({
+    resolve: {
+      modules: [path.resolve(__dirname, 'src'), 'node_modules'],
+    },
+  });
+};
---
+++ src/components/Gallery.js
@@ -0,0 +1,122 @@
+import React from 'react';
+import styled from 'react-emotion';
+import Img from 'gatsby-image';
+import Lightbox from 'react-images';
+
+const Wrapper = styled.div`
+  display: grid;
+  grid-template-columns: repeat(auto-fit, minmax(400px, 1fr));
+  width: 100%;
+`;
+
+const Item = styled.div`
+  position: relative;
+  &:before {
+    content: '';
+    display: block;
+    padding-top: 100%;
+  }
+`;
+
+const Content = styled.div`
+  height: 100%;
+  left: 0;
+  position: absolute;
+  top: 0;
+  width: 100%;
+
+  a {
+    color: #fff;
+    height: 100%;
+    left: 0;
+    opacity: 0;
+    padding: 2rem;
+    position: absolute;
+    top: 0;
+    width: 100%;
+    z-index: 10;
+    transition: all 0.3s ease-in-out;
+    text-decoration: none;
+
+    &:hover {
+      color: #fff;
+      opacity: 1;
+      text-decoration: none;
+    }
+  }
+`;
+
+const ImageWrapper = styled.div`
+  > div {
+    height: 100%;
+    left: 0;
+    position: absolute !important;
+    top: 0;
+    width: 100%;
+
+    > div {
+      position: static !important;
+    }
+  }
+`;
+
+class Gallery extends React.Component {
+  constructor(props) {
+    super(props);
+    
+    this.state = {
+      lightbox: false,
+      photos: props.photos.map(photo => Object.assign({ src: photo.node.frontmatter.cover.childImageSharp.fluid.originalImg })),
+    };
+  }
+
+  gotoPrevLightboxImage() {
+    const { photo } = this.state;
+    this.setState({ photo: photo - 1 });
+  }
+
+  gotoNextLightboxImage() {
+    const { photo } = this.state;
+    this.setState({ photo: photo + 1 });
+  }
+
+  openLightbox(photo, event) {
+    event.preventDefault();
+    this.setState({ lightbox: true, photo });
+  }
+
+  closeLightbox() {
+    this.setState({ lightbox: false });
+  }
+
+  render() {
+    console.log(this.props.photos[0].node.frontmatter.cover.childImageSharp.fluid);
+
+    return (
+      <Wrapper>
+        {this.props.photos.map((photo, i) => {
+          return (
+            <Item key={photo.node.fields.slug} onClick={e => this.openLightbox(i, e)}>
+              <Content>
+                <ImageWrapper>
+                  <Img fluid={photo.node.frontmatter.cover.childImageSharp.fluid} />
+                </ImageWrapper>
+              </Content>
+            </Item>
+          );
+        })}
+        <Lightbox
+          backdropClosesModal
+          images={this.state.photos}
+          currentImage={this.state.photo}
+          isOpen={this.state.lightbox}
+          onClickPrev={() => this.gotoPrevLightboxImage()}
+          onClickNext={() => this.gotoNextLightboxImage()}
+          onClose={() => this.closeLightbox()}
+        />
+      </Wrapper>
+    );
+  }
+}
+
+export default Gallery;
